0b461ca4723b67c7488366cf5d9cadd14ef280e8
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2023-03-31 13:58:23 +0530

    5ire Chain Website work initialised

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="heading">
+        <h1>5ire Chain Website</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
